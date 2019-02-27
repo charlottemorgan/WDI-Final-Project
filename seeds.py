3a8aede847d72027e330ef8a7b180865028f4263
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-02-27 13:40:35 +0000

    added admin user to seeds

--- seeds.py
+++ seeds.py
@@ -7,4 +7,16 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    
+    admin, errors = user_schema.load({
+        'username': 'admin',
+        'email': 'admin@admin.com',
+        'password': 'admin123',
+        'password_confirmation': 'admin123'
+    })
+
+    if errors:
+        raise Exception(errors)
+
+    db.session.add(admin)
+
+    db.session.commit()
